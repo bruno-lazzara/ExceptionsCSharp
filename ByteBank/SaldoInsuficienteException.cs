28fd8738e0ea2dad00960e0180f585c63576a40a
Bruno Lazzara Queiroz <lazzara.bruno@gmail.com>
2022-02-15 21:55:52 -0300

    Adicionar arquivos de projeto.

---
+++ ByteBank/SaldoInsuficienteException.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ByteBank
+{
+    public class SaldoInsuficienteException : Exception
+    {
+        public double Saldo { get; }
+        public double ValorSaque { get; }
+
+        //Construtor sem argumentos
+        public SaldoInsuficienteException()
+        {
+        }
+
+        // Este construtor de dois argumentos chama o construtor com o argumento string message.
+        public SaldoInsuficienteException(double saldo, double valorSaque) : this("Tentativa de saque do valor de " + valorSaque + " em uma conta com saldo de " + saldo)
+        {
+            Saldo = saldo;
+            ValorSaque = valorSaque;
+        }
+
+        // Construtor com argumentos
+        public SaldoInsuficienteException(string message) : base(message)
+        {
+        }
+    }
+}
